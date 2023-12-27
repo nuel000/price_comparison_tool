6d29aa8391f29415219eb1c89e6c1b9a4f6b22ac
Emmanuel Momoh <momohemmanuel370@gmail.com>
2023-12-27 23:21:19 +0100

    new commit

--- price_comparison/wsgi.py
+++ price_comparison/wsgi.py
@@ -9,8 +9,15 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'price_comparison.settings')
+
+import os
+from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "price_comparison.settings")
 
 application = get_wsgi_application()
+application = WhiteNoise(application)
+application.add_files(os.path.join(os.path.dirname(__file__), "comparer/static"), prefix="static/")
+
